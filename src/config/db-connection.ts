175eda5fb13d8fa4bd91ce214fad34c90ad5a0cb
Vinh Nguyen <vinhnguyentan99@gmail.com>
2024-09-11 16:49:37 +0700

    chore: change password of db

--- src/config/db-connection.ts
+++ src/config/db-connection.ts
@@ -5,7 +5,7 @@ export const AppDataSource = new DataSource({
   host: 'localhost',
   port: 5432,
   username: 'me',
-  password: '',
+  password: '1',
   database: 'todo',
   synchronize: true,
   entities: ["src/entity/*.ts"],
